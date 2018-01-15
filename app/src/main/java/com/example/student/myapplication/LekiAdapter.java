376fc61a29dc2d74a50b160df1b44f1fe119575b
unknown <student@IBLAB19N-13.ib.polsl.pl>
2018-01-15 16:56:09 +0100

    Koniec

--- app/src/main/java/com/example/student/myapplication/LekiAdapter.java
+++ app/src/main/java/com/example/student/myapplication/LekiAdapter.java
@@ -1,8 +1,90 @@
 package com.example.student.myapplication;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * Created by student on 2018-01-15.
  */
 
-public class LekiAdapter {
+public class LekiAdapter extends RecyclerView.Adapter<LekiAdapter.ViewHolder> {
+
+    private ArrayList<Leki> lekis = new ArrayList<>();
+    private OnClickPleace listener;
+
+    LekiAdapter(ArrayList<Leki> lekis , OnClickPleace listener)
+    {
+        this.lekis = lekis;
+        this.listener = listener;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemLayoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, null);
+
+        return new ViewHolder(itemLayoutView, listener);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.setCost(lekis.get(position).getPrice());
+        holder.setName(lekis.get(position).getName());
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return lekis.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.lek)
+        TextView name;
+
+        @BindView(R.id.ustaw_dawke)
+        TextView dawka;
+
+        @BindView(R.id.wybierz)
+        Button Wybrane;
+
+        @OnClick(R.id.choose_btn)
+        void onNameClick() {
+
+                Wybrane.setText("wybrano");
+
+        }
+        private OnClickPleace listener;
+
+
+
+
+        public ViewHolder(View itemView, OnClickPleace listener) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+            this.listener = listener;
+            itemView.setOnClickListener(this::OnClick);
+        }
+        private void setName(String nazwa){name.setText(nazwa);}
+        private void setCost(String cena) {dawka.setText(cena);}
+
+
+        public void OnClick(View view)
+        {
+            listener.onClickPlace(view, getAdapterPosition());
+
+        }
+    }
 }
--- app/src/main/java/com/example/student/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/student/myapplication/Main2Activity.java
@@ -1,13 +1,20 @@
 package com.example.student.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class Main2Activity extends AppCompatActivity {
+    @BindView(R.id.recycler)
+    RecyclerView recView;
+    String lekiText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +22,10 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         ButterKnife.bind(this);
 
-        ArrayList<Leki> lekis = new ArrayList<>();
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recView.setLayoutManager(layoutManager);
+
+        final ArrayList<Leki> lekis = new ArrayList<>();
         {
             lekis.add(new Leki("apap","100 mg"));
             lekis.add(new Leki("paracetamol","80 mg"));
@@ -23,16 +33,18 @@ public class Main2Activity extends AppCompatActivity {
             lekis.add(new Leki("gripex","100 mg"));
         }
 
+        OnClickPleace listener = (view, position) -> {
 
+            lekiText = lekis.get(position).getName();
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("CoffeeName", lekiText);
+            intent.putExtras(bundle);
+            startActivity(intent);
+        };
 
-
-
-
-
-
-
-
-
+        LekiAdapter lekiAdapter = new LekiAdapter(lekis , listener);
+        recView.setAdapter(lekiAdapter);
 
 
 
